2cd1bb13dca5a15767080992956519494a4236ca
AayushOjha01 <ojhayush2004@gmail.com>
2025-05-04 14:07:01 +0545

    added views to manage courses with UI

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -31,7 +31,8 @@ urlpatterns = [
         'accounts/logout/',
         auth_views.LogoutView.as_view(),
         name = 'logout',
-    )
+    ),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
